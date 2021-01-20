21bb556527d69869e65aa8e46d72de7b22d1f5be
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-01-20 11:41:11 +0100

    JavaFX (25) - pierwsza apka w Java FX - wyświetla listę rzeczy do zrobienia - Singleton (4): dodanie Dialog Pane (2) - z funkcjonalnością

--- src/m06_application_singleton/DialogController.java
+++ src/m06_application_singleton/DialogController.java
@@ -1,4 +1,29 @@
 package m06_application_singleton;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import m06_application_singleton.datamodel.TodoData;
+import m06_application_singleton.datamodel.TodoItem;
+
+import java.time.LocalDate;
+
 public class DialogController {
+    @FXML
+    private TextField shortDescriptionField;
+    @FXML
+    private TextArea detailsArea;
+    @FXML
+    private DatePicker deadlinePicker;
+
+    public TodoItem processResults(){
+        String shortDescription = shortDescriptionField.getText().trim();
+        String details = detailsArea.getText().trim();
+        LocalDate deadline = deadlinePicker.getValue();
+
+        TodoItem newItemOnList = new TodoItem(shortDescription, details, deadline);
+        TodoData.getInstance().addTodoItem(newItemOnList);
+        return newItemOnList;
+    }
 }
--- src/m06_application_singleton/datamodel/TodoData.java
+++ src/m06_application_singleton/datamodel/TodoData.java
@@ -88,4 +88,8 @@ public class TodoData {
             }
         }
     }
+
+    public void addTodoItem(TodoItem item){
+        todoItems.add(item);
+    }
 }
