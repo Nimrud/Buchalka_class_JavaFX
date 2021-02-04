821d820705bb5829d38fd1cb25a2b59fcb941d16
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-02-04 21:53:58 +0100

    JavaFX (40) - challenge (5)

--- src/m09_challenge/Controller.java
+++ src/m09_challenge/Controller.java
@@ -1,10 +1,10 @@
 package m09_challenge;
 
 import javafx.application.Platform;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.ButtonType;
+import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TableView;
 import m09_challenge.datamodel.Contact;
 import m09_challenge.datamodel.ContactData;
@@ -15,18 +15,12 @@ public class Controller {
     @FXML
     private TableView<Contact> contactTableView;
 
-    // tymczasowo:
-    private ObservableList<Contact> contacts;
-
     public void initialize(){
-
-        Contact contact1 = new Contact("Jan", "Kowalski", "509654987", "brak notatek");
-        contacts.add(contact1);
-
-        contactTableView.setItems(contacts);
+        contactTableView.setItems(ContactData.getInstance().getContacts());
+        contactTableView.getSelectionModel().setSelectionMode(SelectionMode.SINGLE);
     }
 
-    public void deleteItem(Contact contact){
+    public void deleteContact(Contact contact){
         Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
         alert.setTitle("Delete contact");
         alert.setHeaderText("Deleting contact: " + contact.toString());
