89475047de1705a99fc56932272102e31c474a9d
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-02-07 22:46:51 +0100

    JavaFX (42) - challenge (7): zakodowanie opcji dodania kontaktu (z nowym oknem dialogowym)

--- src/m09_challenge/Controller.java
+++ src/m09_challenge/Controller.java
@@ -2,13 +2,12 @@ package m09_challenge;
 
 import javafx.application.Platform;
 import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.ButtonType;
-import javafx.scene.control.SelectionMode;
-import javafx.scene.control.TableView;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.*;
 import m09_challenge.datamodel.Contact;
 import m09_challenge.datamodel.ContactData;
 
+import java.io.IOException;
 import java.util.Optional;
 
 public class Controller {
@@ -29,6 +28,33 @@ public class Controller {
         Optional<ButtonType> result = alert.showAndWait();
         if (result.isPresent() && (result.get() == ButtonType.OK)){
             ContactData.getInstance().deleteContact(contact);
+        }
+    }
+
+    @FXML
+    public void newContactDialog(){
+        Dialog<ButtonType> dialog = new Dialog<>();
+        dialog.initOwner(contactTableView.getScene().getWindow());
+        dialog.setTitle("Add a new contact");
+
+        FXMLLoader loader = new FXMLLoader();
+        loader.setLocation(getClass().getResource("newContact.fxml"));
+        try{
+            dialog.getDialogPane().setContent(loader.load());
+        } catch (IOException e){
+            System.out.println("Failed to load new Dialog Window");
+            e.printStackTrace();
+            return;
+        }
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CANCEL);
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.OK);
+
+        Optional<ButtonType> result = dialog.showAndWait();
+        if (result.isPresent() && (result.get() == ButtonType.OK)){
+            NewContactController controller = loader.getController();
+            Contact newContact = controller.processNewContact();
+            contactTableView.getSelectionModel().select(newContact);
         }
     }
 
