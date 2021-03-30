ae6b14c3dba206739d561cca7fe7584cce512a4c
Krzysztof Jaczewski <krzysztof.jaczewski@op.pl>
2021-03-31 01:54:42 +0200

    JavaFX (48) - threads (2): metoda działająca, ale nierekomendowana

--- src/m10_threads/Controller.java
+++ src/m10_threads/Controller.java
@@ -1,24 +1,42 @@
 package m10_threads;
 
+import javafx.application.Platform;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.concurrent.Task;
+import javafx.fxml.FXML;
+import javafx.scene.control.ListView;
 
 public class Controller {
     private Task<ObservableList<String>> task;
+    @FXML
+    private ListView listView;
 
     public void initialize(){
         task = new Task<ObservableList<String>>() {
             @Override
             protected ObservableList<String> call() throws Exception {
-                Thread.sleep(1000);  // <- symulacja pozyskania info z bazy danych
-                return FXCollections.observableArrayList(
+                Thread.sleep(500);  // <- symulacja pozyskania info z bazy danych
+                ObservableList<String> employees = FXCollections.observableArrayList(
                         "Adam Kowalski",
                         "Anna Kowalska",
                         "Jan Poniedziałek",
-                        "Maria Konieczna");
+                        "Maria Konieczna",
+                        "Marek Łysek");
+
+                Platform.runLater(new Runnable() {
+                    @Override
+                    public void run() {
+                        listView.setItems(employees);
+                    }
+                });
+                return employees;
             }
         };
     }
 
+    @FXML
+    public void buttonPressed(){
+        new Thread(task).start();
+    }
 }
--- src/m10_threads/Main.java
+++ src/m10_threads/Main.java
@@ -1,7 +1,22 @@
 package m10_threads;
 
-public class Main {
-    public static void main(String[] args) {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
+public class Main extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        stage.setTitle("Threads in JavaFX");
+        stage.setScene(new Scene(root, 300, 500));
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 }
